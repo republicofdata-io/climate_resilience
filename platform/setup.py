e0fc6ccc43f9b4834a20576c52dc3bb947576bce
Olivier Dupuis <olivier@republicofdata.io>
2024-08-29 15:15:44 -0400

    Downgrading version of supabase

--- platform/setup.py
+++ platform/setup.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
             "dagster-gcp==0.24.2",
             "dagster-gcp-pandas==0.24.2",
             "feedparser==6.0.11",
-            "supabase==2.7.3",
+            "supabase==2.5.3",
         ],
     )
