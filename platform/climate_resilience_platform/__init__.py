55aba12ffd331a4de835790abca991fdf428d9b8
Olivier Dupuis <olivier@republicofdata.io>
2024-08-29 14:22:16 -0400

    Adding schedules for mining of conversations

--- platform/climate_resilience_platform/__init__.py
+++ platform/climate_resilience_platform/__init__.py
@@ -7,9 +7,17 @@ from dagster import Definitions
 from .assets.medias import media_feed_assets
 from .assets.social_networks import x_conversation_posts, x_conversations
 from .io_managers import bigquery_io_manager
-from .jobs import refresh_media_feeds_job
+from .jobs import (
+    refresh_media_feeds_job,
+    refresh_social_network_conversations_job,
+    refresh_social_network_posts_job,
+)
 from .resources import supabase_resource, x_resource
-from .schedules import refresh_media_feeds_schedule
+from .schedules import (
+    refresh_media_feeds_schedule,
+    refresh_social_network_conversations_schedule,
+    refresh_social_network_posts_schedule,
+)
 
 # Create temp file for GCP credentials
 AUTH_FILE = "/tmp/gcp_creds.json"
@@ -21,8 +29,16 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = AUTH_FILE
 
 defs = Definitions(
     assets=[*media_feed_assets, x_conversations, x_conversation_posts],
-    jobs=[refresh_media_feeds_job],
-    schedules=[refresh_media_feeds_schedule],
+    jobs=[
+        refresh_media_feeds_job,
+        refresh_social_network_conversations_job,
+        refresh_social_network_posts_job,
+    ],
+    schedules=[
+        refresh_media_feeds_schedule,
+        refresh_social_network_conversations_schedule,
+        refresh_social_network_posts_schedule,
+    ],
     resources={
         "bigquery_io_manager": bigquery_io_manager,
         "supabase_resource": supabase_resource,
--- platform/climate_resilience_platform/schedules/__init__.py
+++ platform/climate_resilience_platform/schedules/__init__.py
@@ -1,7 +1,21 @@
 from dagster import build_schedule_from_partitioned_job
 
-from ..jobs import refresh_media_feeds_job
+from ..jobs import (
+    refresh_media_feeds_job,
+    refresh_social_network_conversations_job,
+    refresh_social_network_posts_job,
+)
 
 refresh_media_feeds_schedule = build_schedule_from_partitioned_job(
     job=refresh_media_feeds_job
+)
+
+refresh_social_network_conversations_schedule = build_schedule_from_partitioned_job(
+    refresh_social_network_conversations_job,
+    minute_of_hour=15,
+)
+
+refresh_social_network_posts_schedule = build_schedule_from_partitioned_job(
+    refresh_social_network_posts_job,
+    minute_of_hour=45,
 )
