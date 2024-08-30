26c6d0fa091dcbe3f20c98dceb6854773f37efaa
Olivier Dupuis <olivier@republicofdata.io>
2024-08-30 16:14:18 -0400

    Cleaning up pre-install script

--- dagster_cloud_pre_install.sh
+++ dagster_cloud_pre_install.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-
-# Update package list and install required build tools
-sudo apt-get update
-
-# Install g++ and other essential build tools
-sudo apt-get install -y \
-    build-essential \
-    g++ \
-    libffi-dev \
-    libssl-dev \
-    python3-dev
+apt-get install -y \
+  build-essential \
+  g++ \
+  libffi-dev \
+  libssl-dev \
+  python3-dev
