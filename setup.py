890f3e31742e7b5c85a0415f4b3aa5bc4c39233c
Olivier Dupuis <olivier@republicofdata.io>
2024-09-05 12:28:48 -0400

    Typo

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
             "dagster-gcp==0.24.2",
             "dagster-gcp-pandas==0.24.2",
             "feedparser==6.0.11",
-            "lanchain-core==0.2.38",
+            "langchain-core==0.2.38",
             "langchain-openai==0.1.23",
             "spacy==3.7.5",
             "supabase==2.5.3",
