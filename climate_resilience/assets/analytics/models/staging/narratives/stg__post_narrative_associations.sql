19efcacad66d19397116e9ddc8bff230aa3fa2db
Olivier Dupuis <olivier@republicofdata.io>
2025-02-28 12:02:07 -0500

    Surfacing new taxonomy in analytics assets

--- climate_resilience/assets/analytics/models/staging/narratives/stg__post_narrative_associations.sql
+++ climate_resilience/assets/analytics/models/staging/narratives/stg__post_narrative_associations.sql
@@ -10,13 +10,17 @@ base as (
         cast(post_id as string) as post_natural_key,
         'x' as social_network_source,
         
-        cast(discourse_type as string) as discourse_type,
+        cast(post_type as string) as post_type,
+        cast(discourse_category as string) as discourse_category,
+        cast(discourse_sub_category as string) as discourse_sub_category,
         cast(narrative as string) as narrative,
-        cast(partition_time as timestamp) as discourse_type_partition_ts,
+        cast(justification as string) as justification,
+        cast(confidence as float64) as confidence,
+        cast(partition_time as timestamp) as partition_ts,
     
     from source
 
 )
 
 select * from base
-order by discourse_type_partition_ts, post_natural_key
+order by partition_ts, post_natural_key
